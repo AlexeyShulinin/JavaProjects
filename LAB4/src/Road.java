1528434a9abe034f15a2d7a24f9a5fa3b6824642
Alexey Shulinin <babushka661@gmail.com>
2020-04-15 17:01:16 +0300

    ended LAB4

--- LAB4/src/Road.java
+++ LAB4/src/Road.java
@@ -4,12 +4,14 @@ public class Road {
     private int lines = 5;
     private int countOfCheating = 0;
     private int countOfCars = 0;            //теперь можно узнать о состоянии машины(нарушила или нет),даже она уже съехала с дороги
+    private int MaxSpeed;
     private ArrayList<Car> cars;
     private ArrayList<Thread> carsThreads;
 
     Road(){
         cars = new ArrayList<Car>(51);
         carsThreads = new ArrayList<Thread>(51);
+        this.MaxSpeed = 0;
     }
 
     public void addCar(Car newCar){
@@ -18,6 +20,7 @@ public class Road {
         carsThreads.get(carsThreads.size()-1).start();
         if(newCar.getSpeed() > 60) {
             this.countOfCheating++;
+            if (newCar.getSpeed() > MaxSpeed) MaxSpeed = newCar.getSpeed();
             carsThreads.get(carsThreads.size()-1).interrupt();
             cars.remove(cars.size()-1);
             carsThreads.remove(carsThreads.size()-1);
@@ -28,6 +31,7 @@ public class Road {
     public void initCountOfCheating(){
         this.countOfCheating = 0;
         this.countOfCars = 0;
+        this.MaxSpeed = 0;
     }
 
     public int retAmount(){
@@ -36,6 +40,7 @@ public class Road {
 
     public void tellAboutCheating(){
         System.out.println("Кол-во нарушений: "+ this.countOfCheating);
+        System.out.println("Макс скорость: "+ this.MaxSpeed);
         carsThreads.removeAll(carsThreads);
         cars.removeAll(cars);
     }
