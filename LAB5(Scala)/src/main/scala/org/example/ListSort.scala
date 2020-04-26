8e02b12400a09e7e2edbbe59808144e4a8403f12
Alexey Shulinin <babushka661@gmail.com>
2020-04-26 21:33:46 +0300

    First program in Scala
    Two func with list
    This func sotring list by tailRec and without tailrec

---
+++ LAB5(Scala)/src/main/scala/org/example/ListSort.scala
@@ -0,0 +1,46 @@
+package org.example
+
+import scala.annotation.tailrec
+
+object ListSort {
+
+  def sortListTailRec(lst:List[Double]):List[Double] = {
+      def insert(x: Double, sorted:List[Double]): List[Double] = sorted match {
+        case Nil => x :: Nil //пустой лист
+        case h :: t => if (x < h) x :: sorted else h :: insert(x, t) //сравниваем позициюю с головой x
+      }
+
+      @tailrec
+      def helper(sorted:List[Double], unsorted:List[Double]):List[Double] =
+        unsorted match {
+          case Nil => sorted
+          case h::t => helper(insert(h, sorted), t)
+        }
+
+      helper(Nil, lst)
+  }
+
+
+  def sortList(list: List[Int]): List[Int] = list match {
+    case head :: tail =>
+      val left = tail.filter(_ < head)
+      val right = tail.filter(_ >= head)
+      sortList(left) ++ List(head) ++ sortList(right)
+    case Nil => Nil
+  }
+
+
+  def main(args:Array[String]): Unit ={
+
+/*
+    val message = computeMessage()
+
+    val number = computeNumber()
+
+    println(message + number);
+*/
+
+    sortListTailRec(List(1,53,2,22,5,12))
+    sortList(List(1,53,2,22,5,12))
+  }
+}
